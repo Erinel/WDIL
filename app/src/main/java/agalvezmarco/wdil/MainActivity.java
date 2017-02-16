f1ba9e80124aee4d1ba3f42250941d3469de4730
Erinel <agalvezmarcodev@gmail.com>
2017-02-16 19:45:34 +0100

    Added English Translation

--- app/src/main/java/agalvezmarco/wdil/MainActivity.java
+++ app/src/main/java/agalvezmarco/wdil/MainActivity.java
@@ -3,7 +3,6 @@ package agalvezmarco.wdil;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
